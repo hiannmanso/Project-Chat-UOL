8d811eb69cb72996e0f4d2fb4729025a99151ff5
Hiann <hiannmanso@email.com>
2022-02-09 16:24:33 -0300

    importando users para minha sidebar

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let user = ''
+let usersGlobais = []
+
 function showSideBar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.classList.remove('hidden')
@@ -23,15 +26,13 @@ function choiceVisibilit(person){
 }
 
 
-let user = ''
+loginUser()
 function loginUser(){
     user = prompt('seu nome pf:')
-    return user
+    sendUser(user)
 }
-loginUser()
 
 sendUser()
-
 function sendUser(){
     //verifica se tem alguem logado na API com o seu usuário, 
     //se o status for 200 nao tem se for 400 tem.
@@ -42,30 +43,65 @@ function sendUser(){
         data:{
             name :user
         }
-    }).then((yourUserName)=>{
-        console.log(yourUserName);
-        console.log(yourUserName.status);
-    }).catch(()=>{
-        console.log('something is wrong');
+    }).then((isLogged)=>{
+        if (isLogged.status == 200) {
+            getInfoUsers()
+            setInterval(isStillLogged,5000)
+        }
+        else{
+            loginUser()
+        }
+    }).catch((error)=>{
+        console.log(error);
     })
 }
-
-
-setInterval(isStillLogged,5000)
+//testa se o usuário aidna está logado
 function isStillLogged() {
-    let url = 'ttps://mock-api.driven.com.br/api/v4/uol/status'
+    let url = 'https://mock-api.driven.com.br/api/v4/uol/status'
     axios({
         method:'post',
         url: url,
         data:{
             name :user
         }
-    }).then((yourUserName)=>{
-        console.log(yourUserName);
-        console.log(yourUserName.status);
-        
-    }).catch(()=>{
-        console.log(user);
+    }).then()
+    .catch((error)=>{
+        console.log(error);
         console.log('something is wrong');
     })
 }
+
+//recebe as informações dos usuários
+function getInfoUsers() {
+    let url = 'https://mock-api.driven.com.br/api/v4/uol/participants ' 
+
+    axios({
+        method:'get',
+        url:url,
+    }).then(item=>{
+        //pega as informações de users e joga tentro de usersGlobais
+        for (let cont = 0; cont < item.data.length; cont++) {
+            usersGlobais[cont] =item.data[cont] 
+           
+        }
+        showUsersSideBar()
+    }).catch(error=>{
+        console.log(error);
+    })
+}
+
+//coloca os users na sidebar
+function showUsersSideBar(){
+    for (let index = 0; index < usersGlobais.length; index++) {
+        let usersSideBar = document.querySelector('.users-sidebar')
+        usersSideBar.innerHTML += 
+        `<div class="user-sidebar">
+            <ion-icon class="icon all" name="people-sharp"></ion-icon>
+            <div class="name-user-sidebar">
+                <p >${usersGlobais[index].name}</p>
+                <ion-icon class="checkbox hidden " name="checkmark-outline"></ion-icon>
+            </div>
+        </div>`
+        
+    }
+ }
