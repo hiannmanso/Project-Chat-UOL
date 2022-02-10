e750b6475558dac14b28380a7d7ef7a8fc9331cf
Hiann <hiannmanso@email.com>
2022-02-10 16:07:48 -0300

    send msg (making)

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let user = ''
+let messageTo =''
+let textMsg=''
+let typemessage=''
 let usersGlobais = []
+
+
 
 function showSideBar() {
     const sidebar = document.querySelector('.sidebar')
@@ -8,63 +13,68 @@ function showSideBar() {
     complementSidebar.classList.remove('hidden')
 
 }
-function closeSideBar(){
+function closeSideBar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.classList.add('hidden')
     const complementSidebar = document.querySelector('.complement-sidebar')
     complementSidebar.classList.add('hidden')
 }
-function choicePerson(personChoiced){
+function choicePerson(personChoiced) {
     let persons = document.querySelectorAll('.user-sidebar')
-    let hiddenCheckboxs= document.querySelectorAll('.user-sidebar .top')
+    let hiddenCheckboxs = document.querySelectorAll('.user-sidebar .top')
     for (let index = 0; index < persons.length; index++) {
         hiddenCheckboxs[index].classList.add('hidden')
     }
+    messageTo = personChoiced.querySelector('.name-user-sidebar p').innerHTML
     personChoiced.querySelector('.user-sidebar .top').classList.remove('hidden')
 }
 
-function choiceVisibilit(person){
+function choiceVisibilit(visibilitChoice) {
     let closeAllPadLocks = document.querySelectorAll('.choice-visibilit .icon')
     let hiddenAllCheckBox = document.querySelectorAll('.name-user-sidebar .bottom')
     for (let index = 0; index < closeAllPadLocks.length; index++) {
-        closeAllPadLocks[index].setAttribute('name','lock-closed-outline')
+        closeAllPadLocks[index].setAttribute('name', 'lock-closed-outline')
         hiddenAllCheckBox[index].classList.add('hidden')
-        
+
     }
-    let padlock = person.querySelector('.padlock') 
-    padlock.setAttribute('name','lock-open-outline')
-    let checkbox = person.querySelector('.checkbox')
+    let padlock = visibilitChoice.querySelector('.padlock')
+    padlock.setAttribute('name', 'lock-open-outline')
+    let checkbox = visibilitChoice.querySelector('.checkbox')
     checkbox.classList.remove('hidden')
+    
+    typemessage = visibilitChoice
+    console.log(typemessage);
+    console.log(typemessage.name);
 }
 
 
 loginUser()
-function loginUser(){
+function loginUser() {
     user = prompt('seu nome pf:')
     sendUser(user)
 }
 
 sendUser()
 //envia o usuário que você decidiu logar
-function sendUser(){
+function sendUser() {
     //verifica se tem alguem logado na API com o seu usuário, 
     //se o status for 200 nao tem se for 400 tem.
-    let url = 'https://mock-api.driven.com.br/api/v4/uol/participants ' 
+    let url = 'https://mock-api.driven.com.br/api/v4/uol/participants'
     axios({
-        method:'post',
+        method: 'post',
         url: url,
-        data:{
-            name :user
+        data: {
+            name: user
         }
-    }).then((isLogged)=>{
+    }).then((isLogged) => {
         if (isLogged.status == 200) {
             getInfoUsers()
-            setInterval(isStillLogged,5000)
+            setInterval(isStillLogged, 5000)
         }
-        else{
+        else {
             loginUser()
         }
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log(error);
     })
 }
@@ -72,71 +82,85 @@ function sendUser(){
 function isStillLogged() {
     let url = 'https://mock-api.driven.com.br/api/v4/uol/status'
     axios({
-        method:'post',
+        method: 'post',
         url: url,
-        data:{
-            name :user
+        data: {
+            name: user
         }
     }).then()
-    .catch((error)=>{
-        console.log(error);
-        console.log('something is wrong');
-    })
+        .catch((error) => {
+            console.log(error);
+            console.log('something is wrong');
+        })
 }
 
 //recebe as informações dos usuários
 function getInfoUsers() {
-    let url = 'https://mock-api.driven.com.br/api/v4/uol/participants ' 
+    let url = 'https://mock-api.driven.com.br/api/v4/uol/participants'
 
     axios({
-        method:'get',
-        url:url,
-    }).then(item=>{
+        method: 'get',
+        url: url,
+    }).then(item => {
         //pega as informações de users e joga tentro de usersGlobais
         for (let cont = 0; cont < item.data.length; cont++) {
-            usersGlobais[cont] =item.data[cont] 
-           
+            usersGlobais[cont] = item.data[cont]
+
         }
         showUsersSideBar()
-    }).catch(error=>{
+    }).catch(error => {
         console.log(error);
     })
 }
 
 //coloca os users na sidebar
-function showUsersSideBar(){
+function showUsersSideBar() {
     for (let index = 0; index < usersGlobais.length; index++) {
         let usersSideBar = document.querySelector('.users-sidebar')
-        usersSideBar.innerHTML += 
-        `<div class="user-sidebar" onclick='choicePerson(this)'>
+        usersSideBar.innerHTML +=
+            `<div class="user-sidebar" onclick='choicePerson(this)'>
             <ion-icon class="icon all" name="people-sharp"></ion-icon>
             <div class="name-user-sidebar">
                 <p >${usersGlobais[index].name}</p>
                 <ion-icon class="checkbox hidden top" name="checkmark-outline"></ion-icon>
             </div>
         </div>`
-        
+
     }
- }
+}
 
-
- showMessages() 
+//render messages na tela
+showMessages()
 function showMessages() {
     let url = 'https://mock-api.driven.com.br/api/v4/uol/messages'
     axios({
-        method:'get',
-        url: url
-    }).then((itens)=>{
-        console.log(itens);
-        for (let index = 0; index < itens.length; index++) {
-            if (itens.data.type[index] =='status') {
-                console.log('mensagem de status');
-            }if (itens.data.type =='message') {
-                console.log('mensagem');
-            }if (itens.data.type =='private_message') {
-                console.log('mensagem privada');
-            }
-            
+        method: 'get',
+        url: url,
+    }).then((response) => {
+        console.log(response);
+        for (let index = 0; index < response.data.length; index++) {
+            let caminhoMsg =response.data[index]
+            let containerMessages = document.querySelector(".container-mesage")
+            containerMessages.innerHTML +=
+                `<div class="mesage">
+                    <h1>(${caminhoMsg.time}) <b>${caminhoMsg.from}</b> para ${caminhoMsg.to}: ${caminhoMsg.text}</h1>
+                </div>`
+        
         }
     })
+}
+
+function sendMessage() {
+    let url ='https://mock-api.driven.com.br/api/v4/uol/participants'
+    let msg = document.querySelector('.msg').value
+    // axios({
+    //     method:'get',
+    //     url: url,
+    //     data:{
+    //         from:user,
+    //         to:messageTo,
+    //         text:msg,
+    //         type:,
+    //     }
+    // })
 }
